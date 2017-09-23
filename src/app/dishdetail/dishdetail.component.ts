3d9b12b51f3a23ce04574fd6e6626cd03c19ddb9
gohminghui88 <gohminghui88@gmail.com>
2017-09-24 04:46:13 +0800

    REST Save

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -28,6 +28,7 @@ export class DishdetailComponent implements OnInit {
 	commentForm: FormGroup;
 	comment: Comment;
 	dish: Dish;
+	dishcopy = null;
 	dishIds: number[];
 	prev: number;
 	next: number;
@@ -49,9 +50,9 @@ export class DishdetailComponent implements OnInit {
     //this.dishservice.getDish(id).subscribe(dish => this.dish = dish);
 	this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params
-      .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); },
-      errmess => this.errMess = <any>errmess);
+      .switchMap((params: Params) => { return this.dishservice.getDish(+params['id']); })
+      .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); },
+          errmess => { this.dish = null; this.errMess = <any>errmess; });
 	
 	
   }
@@ -82,15 +83,17 @@ export class DishdetailComponent implements OnInit {
 
   onSubmit(data: any) {
 	  
-	  //this.route.params
-	//	.switchMap((params: Params) => this.temp = +params['id'])
-	//	.subscribe(commentForm => {this.submittedForm = formsValue;})
+	  this.comment = this.commentForm.value;
+	  this.comment.date = new Date().toISOString();
+	  
+	  this.dishcopy.comments.push(this.comment);
+      this.dishcopy.save()
+      .subscribe(dish => { this.dish = dish; console.log(this.dish); });
 	
-	data.value.date = new Date().toISOString();
 	
-	this.dish.comments.push(data.value);
+	//this.dish.comments.push(data.value);
 	
-	console.log(data.value);	
+	console.log(this.comment);	
 	
     this.commentForm.reset();
   }
