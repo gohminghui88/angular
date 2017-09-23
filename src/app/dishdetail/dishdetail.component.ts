e857ccd7ff27f10c99df8699a123aefe49df7dc1
gohminghui88 <gohminghui88@gmail.com>
2017-09-24 06:02:51 +0800

    Animations 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, animate, transition } from '@angular/animations';
+//import { trigger, state, style, animate, transition } from '@angular/animations';
 
 import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
@@ -15,22 +15,21 @@ import {MdInputModule} from '@angular/material';
 
 import { Comment } from '../shared/comment';
 
+import { visibility } from '../animations/app.animation';
+import { flyInOut, expand } from '../animations/app.animation';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.css'], 
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
   animations: [
-    trigger('visibility', [
-        state('shown', style({
-            transform: 'scale(1.0)',
-            opacity: 1
-        })),
-        state('hidden', style({
-            transform: 'scale(0.5)',
-            opacity: 0
-        })),
-        transition('* => *', animate('0.5s ease-in-out'))
-    ])
+    visibility(), 
+	flyInOut(), 
+	expand()
   ]
 })
 
